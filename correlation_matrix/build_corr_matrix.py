6f047e239fb164a6a51c1b135389619ea82ce63b
kenchan323 <kenchan323@hotmail.com>
2019-12-25 16:00:21 +0000

    Script for clustering DJ constituents and tsne visualisation

--- correlation_matrix/build_corr_matrix.py
+++ correlation_matrix/build_corr_matrix.py
@@ -5,22 +5,43 @@ import os
 dir_project = os.path.dirname(__file__)
 dir_csv_folders = os.path.join(dir_project, "dj_mem_ret_yahoo_finance")
 
-def parse_csv_returns(dir_returns_files=dir_csv_folders, rho_matrix=False):
+def parse_csv_returns(dir_returns_files=dir_csv_folders, rho_matrix=False, start_date=None, end_date=None, use_name=False):
     for root, dirs, files in os.walk(dir_csv_folders):
         csv_files = files # list of csv file names
+        break
     df_output = pd.DataFrame()
 
+    if use_name:
+        # Load ticker to stock name mapping
+        series_ticker_map = pd.read_csv(os.path.join(dir_returns_files, "mapping", "ticker_name.csv"), index_col=0)[
+            "Name"]
+        dict_ticker_name_map = series_ticker_map.to_dict()
+
     for csv_file in csv_files:
+        if csv_file == "ticker_name.csv":
+            continue
         ticker = csv_file.split(".")[0]
         # Set index col to be date
-        df_stock = pd.read_csv(os.path.join(dir_returns_files, csv_file), index_col=0)
-        df_output[ticker] = df_stock["Close"]
+        df_stock = pd.read_csv(os.path.join(dir_returns_files, csv_file), index_col=0, parse_dates=True)
+
+        if use_name:
+            # We get the stock name instead of using its ticker
+            df_output[dict_ticker_name_map[ticker]] = df_stock["Close"]
+        else:
+            df_output[ticker] = df_stock["Close"]
+
+    if start_date != None:
+        df_output = df_output.loc[start_date:]
+
+    if end_date != None:
+        df_output = df_output.loc[:end_date]
+
+    max_date = df_output.index.max()
+    min_date = df_output.index.min()
+    print("Based on time period {} to {}".format(min_date.strftime("%Y-%m-%d"), max_date.strftime("%Y-%m-%d")))
 
     # Turn daily close prices to daily percentage return
     df_output = df_output.pct_change()
     if rho_matrix:
         return df_output.corr()
     return df_output
-
-df_returns = parse_csv_returns()
-df_corr = df_returns.corr()
---
+++ correlation_matrix/run_corr_matrix.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import datetime as dt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+import sys
+
+from sklearn.cluster import AgglomerativeClustering
+from scipy.spatial.distance import squareform
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.manifold import TSNE
+try:
+    import build_corr_matrix as build_corr
+except ImportError:
+    sys.path.insert(0, r"C:\dev\k_personal_projects\correlation_matrix")
+    import build_corr_matrix as build_corr
+
+def ultra_metric(rho):
+    '''
+    Turning correlation coefficient into a distance measure that satisfies strong triangle inequality
+    :param rho:
+    :return:
+    '''
+    return np.sqrt(2*(1-rho))
+
+start_date = None
+end_date = None
+#start_date = dt.datetime(2019, 6, 1)
+#end_date = dt.datetime(2019, 12, 1)
+
+# Cluster count
+n_clusters = 15
+# Perplexity hyperparameter for TSNE
+perp = 5
+
+# Fetching a correlation matrix
+df_corr = build_corr.parse_csv_returns(rho_matrix=True, start_date=start_date, end_date=end_date, use_name=True).dropna()
+# Apply ultra metric calculation for distance matrix
+df_corr_distance = df_corr.loc[:].apply(ultra_metric)
+# 2D array
+ndarray_distance = df_corr_distance.values
+list_tickers = df_corr.index.values
+# Linkage must be average if using a precomputed distance matrix
+cluster = AgglomerativeClustering(n_clusters=n_clusters, affinity="precomputed", linkage="average")
+cluster_labels = cluster.fit_predict(ndarray_distance)
+# Using TSNE to project a set of points based on a precomputed distance matrix
+tsne_projection = TSNE(n_components=2, perplexity=perp, metric="precomputed", random_state=1).fit_transform(ndarray_distance)
+
+ax_1 = plt.figure()
+colour_palette = sns.color_palette("Paired", 36)
+cluster_colours = [colour_palette[x] if x > 0
+                   else (0.5, 0.5, 0.5) for x in cluster_labels]
+
+for i, ticker in enumerate(list_tickers):
+    x = tsne_projection[i][0]
+    y = tsne_projection[i][1]
+    plt.scatter(x, y, color=cluster_colours[i], s=500)
+    plt.text(x + 0.3,  y + 0.3, ticker, fontsize=15)
+    plt.axis("off")
+    if start_date == None and end_date == None:
+        plt.title("DJ Rho Clustering - "
+                  "n_cluster={}, "
+                  "Perplex={}, Metric={}, Linkage=average, 20181226-20191225 ".format(n_clusters, perp, "ultra"))
+    else:
+        plt.title("DJ Rho Clustering - "
+                  "n_cluster={}, "
+                  "Perplex={}, Metric={}, Linkage=average, {}-{}".format(n_clusters, perp, "ultra",
+                                                                         start_date.strftime("%Y%m%d"),
+                                                                         end_date.strftime("%Y%m%d")))
+plt.show()
